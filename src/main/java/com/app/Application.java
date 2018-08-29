a9f223295fd6778ae6bab78fa664c42781631259
WangShaoBin <wangshaobin@arrail-dental.com>
2018-08-29 10:38:08 +0800

    1111

--- src/main/java/com/app/Application.java
+++ src/main/java/com/app/Application.java
@@ -1,14 +1,20 @@
 package com.app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAutoConfiguration   //根据添加的jar包开启自动配置
+//@ComponentScan(value = "com.*") //如果没和application同级就用扫描
 public class Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
     }
 }
